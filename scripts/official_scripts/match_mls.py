909025c87957d7369fa8c9171a095d50d535785e
kafitz <kyle@kylefitz.com>
2013-10-26 19:20:47 -0400

    Added tests for parsing XLS addresses against official database: -If address is not found by direct match: fetches all possible addresses and finds properties with a matching range, generate possible addresses (by 2's) for each and match entry to appropriate choice -Needs improvement: System of which start, end addresses from xls are compared to the db during lookup (order of operations/various combinations)

--- scripts/official_scripts/match_mls.py
+++ scripts/official_scripts/match_mls.py
@@ -63,6 +63,7 @@ def get_street_parameters(row):
         street_type = None
     num_parts = len(name_parts)
 
+    ## STREET ORIENTATION (is this noted in the database?)
     if num_parts > 1:
         # determine if there is an orientation included at the end of the street name
         orientations = ('N', 'S', 'O', 'E')
@@ -97,15 +98,15 @@ def get_street_parameters(row):
         article = None
 
     ## STREET NUMBERS
-    street_parameters['street_number_lower'] = row['no_civique_debut']
-    street_parameters['street_number_upper'] = None
+    lower_street_num = row['no_civique_debut']
+    upper_street_num = None
     if row['no_civique_fin']:
-        street_parameters['street_number_upper'] = row['no_civique_fin']
+        lower_street_num = row['no_civique_fin']
 
     ## SUITE NUMBER
-    street_parameters['suite_num'] = None
+    suite = None
     if row['appartement']:
-        street_parameters['suite_num'] = row['appartement']
+        suite = row['appartement']
 
     ## OUTPUT DICT
     street_nominal = ' '.join(name_parts).upper()
@@ -115,16 +116,15 @@ def get_street_parameters(row):
         'type_code': street_type_code,
         'orientation': orientation,
         'joining_article': article,
-        'article_code': article_code 
+        'article_code': article_code,
+        'street_number_lower': lower_street_num,
+        'street_number_upper': upper_street_num,
+        'suite_num': suite 
         }
     return street_parameters
 
-def db_lookup(table, street_parameters):
-    # create a list of tuples to specifiy the db search criteria
-    sql_criteria = [('b72voie1', street_parameters['street_nominal'].upper()),
-                    ('b72m1', street_parameters['street_number_lower'])]   
-    if street_parameters['type_code']: # street type
-        sql_criteria.append(('b72r1', street_parameters['type_code']))
+def db_search_address(table, sql_criteria):
+    '''Try to find the address in database by exact match'''
     # concatenate the sql string to execute on the database
     # '''SELECT * FROM 'table' WHERE "foo"="132" and "bar"="MAIN"'''
     sql_command = '''SELECT * FROM '{}' WHERE'''.format(table)    
@@ -132,41 +132,96 @@ def db_lookup(table, street_parameters):
         sql_command = sql_command + ''' "{}"="{}"'''.format(criteria[0], criteria[1])
         if index + 1 != len(sql_criteria):
             sql_command = sql_command + " AND"
-    sql_command
-    print sql_command
     db.query(sql_command)
     return db.fetchall()
 
-def parse_query_response(street_parameters):
+def db_match_suite(street_parameters, db_matches):
+    '''If multiple addresses match SQL query, search these for a matching suite number'''
+    pass
+
+def db_search_entire_street(table, street_parameters):
+    '''If exact match fails: search for address in the full range of 
+    addresses for the particular street'''
+    street_name = street_parameters['street_nominal']
+    sql_command = '''SELECT "b72m1", "b72a1" FROM '{}' WHERE "b72voie1"="{}"'''.format(table, street_name)
+    db.query(sql_command)
+    street_number_lower = int(street_parameters['street_number_lower'])
+    street_number_lower = 3895
+    possible_pairs = []
+    for address_pair in sorted(db.fetchall()):
+        # ignore tuples that do not form an address range for a property
+        if address_pair[1] == 0:
+            continue
+        # find all tuple pairs that contain the specified addresses
+        if address_pair[0] <= street_number_lower and address_pair[1] >= street_number_lower:
+            possible_pairs.append(address_pair)
+    matched_address_pair = None
+    for matching_pair in possible_pairs:
+        # generate the range of possible addresses for a certain property (inclusive of the last address)
+        address_range = range(matching_pair[0], matching_pair[1], 2)
+        address_range.append(matching_pair[1])
+        if street_number_lower in address_range:
+            matched_address_pair = matching_pair
+            break
+    if matched_address_pair:
+        street_parameters['street_number_lower'] = matched_address_pair[0]
+        street_parameters['street_number_upper'] = matched_address_pair[1]
+        return street_parameters
+    else:
+        return None
+
+def get_query_response(street_parameters):
     '''Script specific function for matching database response to dict variables'''
-    result = {}
-    r = db_lookup('joined_data', street_parameters)
+    def _map_to_dict(db_entry):
+        result_dict = {}
+        result_dict['street_name'] = db_entry[30]
+        result_dict['start_address'] = db_entry[24]
+        result_dict['street_code'] = db_entry[28]
+        result_dict['end_address'] = db_entry[25]
+        result_dict['land_value'] = db_entry[45]
+        result_dict['building_value'] = db_entry[46]
+        result_dict['total_value'] = db_entry[47]
+        return result_dict
+
+    ## SEARCH FOR XLS ADDRESS NUMBER AS BOTH START AND END (if needed) ADDRESS
+    table_name = 'joined_data'
+    # create a list of tuples to specifiy the db search criteria
+    sql_criteria = [('b72voie1', street_parameters['street_nominal'].upper()),
+                    ('b72m1', street_parameters['street_number_lower'])]   
+    if street_parameters['type_code']: # street type
+        sql_criteria.append(('b72r1', street_parameters['type_code']))
+    r = db_search_address(table_name, sql_criteria)
+    if not r:
+        # create a list of tuples to specifiy the db search criteria
+        sql_criteria = [('b72voie1', street_parameters['street_nominal'].upper()),
+                        ('b72a1', street_parameters['street_number_lower'])]   
+        if street_parameters['type_code']: # street type
+            sql_criteria.append(('b72r1', street_parameters['type_code']))
+        r = db_search_address(table_name, sql_criteria)
+
     if len(r) == 1:
         r = r[0]
-        result['street_name'] = r[30]
-        result['start_address'] = r[24]
-        result['street_code'] = r[28]
-        # result['start_address'] =
-        # result['land_value'] = 
-        # result['building_value'] =
-        # result['total_value'] = 
-
-        # print 'Search:', row
-        # print 'Result:', result
+        result = _map_to_dict(r)
+        print "Address match: {}-{} {}".format(result['start_address'],
+                                            result['end_address'],
+                                            result['street_name'])
         return result
     elif len(r) > 1:
-        # print r
         print "!!!!!MULTIPLE MATCHES"
-        pass
     else:
-        # print r
-        print "!!!!NO MATCHES"
-        pass
+        # search for address between listed start and end street address numbers
+        # listed in database
+        updated_parameters = db_search_entire_street(table_name, street_parameters)
+        if updated_parameters:
+            recursive_result = get_query_response(updated_parameters)
+            print "Found address pair: {}-{} {}".format(updated_parameters['street_number_lower'], updated_parameters['street_number_upper'], updated_parameters['street_nominal'])
+            return recursive_result
+        else:
+            print "No address match found: {} {}".format(street_parameters['street_number_lower'], street_parameters['street_nominal'])
     return None
 
 
 ### MAIN
-
 row_dicts = get_spreadsheet_dict(INPUT_SPREADSHEET)
 print 'Rows for %s:' % (YEAR,), len(row_dicts)
 
@@ -174,8 +229,7 @@ index = 0
 matches = 0
 for row in row_dicts:
     street_parameters = get_street_parameters(row)
-        
-    result = parse_query_response(street_parameters)
+    result = get_query_response(street_parameters)
     street_parts = street_parameters['street_nominal'].split()
     if not result and len(street_parts) > 1:
         # special case: addresses like "25E AVENUE"--attempt scrape for both "25E AVENUE" and "25E"
@@ -188,14 +242,13 @@ for row in row_dicts:
             else:
                 print 'Failure'
 
-
     if result:
         matches += 1
         print 'Matches: {}/{}'.format(matches, index + 1)
         print 'Pct. Matched: ', float(matches) / (index + 1)
         print 'Pct. of total: ', float(index + 1) / len(row_dicts)
     else:
-        print 'Missed:', street_parameters['street_number_lower'] + " " + street_parameters['street_nominal']
+        print 'Missed:', str(street_parameters['street_number_lower']) + " " + street_parameters['street_nominal']
         break
     index += 1
     # if index == 5000:
