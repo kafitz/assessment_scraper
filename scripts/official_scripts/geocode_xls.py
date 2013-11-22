62afe9b4575b06c9c625b671141281911ecd3fe4
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-11-22 14:27:20 -0500

    Hardcoded schema for output database so types are not incorrectly guessed, set python type to Int when possible for database comparisons

--- scripts/official_scripts/geocode_xls.py
+++ scripts/official_scripts/geocode_xls.py
@@ -10,15 +10,28 @@ from unidecode import unidecode
 import geopy
 
 
+def coerce_to_int(possible_float):
+    try:
+        possible_float = int(possible_float)
+        return possible_float
+    except:
+        return possible_float
+
+
+year = '2009'
 g = geopy.geocoders.GoogleV3(domain='maps.google.ca')
 # Open excel
 wb = xlrd.open_workbook('../../data/geocoding/official_mls_2009_sales.xls')
 sh = wb.sheet_by_index(0)
 # Output database
 success_db = dataset.connect('sqlite:///../../data/geocoding/geocoded_xls_input.sqlite')
-successful_geocodes = success_db['2009_sales']
 failed_db = dataset.connect('sqlite:///../../data/geocoding/failed_lookup.sqlite')
-failed_geocodes = failed_db['2009_failed_geocodes']
+failed_geocodes = failed_db[year + '_failed_geocodes']
+# create table with proper schema
+if year + '_sales' not in success_db.tables:
+    success_schema = open('geocode_db.schema').read()
+    success_db.query(success_schema.format(year + '_sales'))
+successful_geocodes = success_db[year + '_sales']    
 
 row_list = []
 for rownum in range(sh.nrows):
@@ -33,26 +46,34 @@ failed_entries = []
 for row in row_list:
     current_row += 1
     print("Row: {0}/{1} ({2:2f}%)".format(current_row, total_rows, float(current_row) / total_rows * 100))
-    search_str = row[6].split(',')[0] + ", " + "Montreal, QC"
+    start_address = str(coerce_to_int(row[3]))
+    street_address = start_address + ' ' + row[6].split(',')[0]
+    search_str = street_address + ", " + "Montreal, QC"
     search_address = unidecode(search_str)
     # Look for entry in success results
+    print '''SELECT id FROM "2009_sales" WHERE nom_complet="%s" AND no_civique_debut="%s"''' % (row[6], start_address)
     try:
-        entry_exists = [x for x in success_db.query('''SELECT id FROM "2009_sales" WHERE input_search="%s"''' % row[0])]
+        entry_exists = [x for x in success_db.query('''SELECT id FROM "2009_sales" WHERE nom_complet="%s" AND no_civique_debut="%s"''' % (row[6], start_address))]
     except:
         entry_exists = False
     # If not there, search in failed_lookups success_db
     if not entry_exists:
         try:
-            entry_exists = [x for x in failed_db.query('''SELECT id FROM "failed_2009_sales" WHERE input_search="%s"''' % row[0])]
+            entry_exists = [x for x in failed_db.query('''SELECT id FROM "failed_2009_sales" WHERE nom_complet="%s" AND no_civique_debut="%s"''' % (row[6], start_address))]
         except:
             entry_exists = False
     # If in neither, attempt to geocode
-    if not entry_exists:    
+    if entry_exists:    
+        print 'Skipped: existing entry found'
+    else:
         lat, lng = (None, None)
         table_update = dict(list(zip(failed_header, row)))
         for entry in table_update:
+            table_update[entry] = coerce_to_int(table_update[entry])
             if table_update[entry] == '':
-                table_update[entry] = 0.0
+                table_update[entry] = None
+            if table_update[entry] == 0:
+                table_update[entry] = None
         try:
             response = g.geocode(search_address, exactly_one=False)
             result = response[0]
@@ -62,9 +83,6 @@ for row in row_list:
             if len(response) != 1:
                 table_update.update({'multiple_matches': 1})
             successful_geocodes.insert(table_update)
-        except geopy.geocoders.googlev3.GeocoderQuotaExceeded:
-            print("Geocoding quota exceeded.")
-            break
         except geopy.geocoders.googlev3.GeocoderQueryError:
             print("Couldn't find address.")
             print(table_update)
@@ -73,9 +91,11 @@ for row in row_list:
             print("Couldn't insert entry, improperly formatted data?")
             print(table_update)
             failed_entries.append(table_update)
-        time.sleep(1.5)
-        if current_row == 20:
+        except geopy.geocoders.googlev3.GeocoderQuotaExceeded:
+            print("Geocoding quota exceeded.")
             break
+        time.sleep(.5)
+
 
 for item in failed_entries:
     print item
